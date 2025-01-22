b5d711fd9e38ddceca7591ed5ce11b3ad1266885
Papatsiri2002 <91860184+Papatsiri2002@users.noreply.github.com>
2025-01-22 09:03:44 +0700

    Prepare for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/HATO_Inner_Child_Quiz/', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
