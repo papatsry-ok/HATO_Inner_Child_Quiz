44deca41eb23dbaab4d931a5644fc1fe785bc492
dtinth on MBP M1 <dtinth@spacet.me>
2025-01-23 01:34:59 +0700

    remove base to be able to deploy to netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/HATO_Inner_Child_Quiz/', 
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
